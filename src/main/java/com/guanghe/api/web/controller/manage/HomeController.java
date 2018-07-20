406e2359fb806b507369b02730ebfd6bd91627a3
yixingwang <987615806@qq.com>
2018-07-20 17:06:48 +0800

    代码跟新

--- src/main/java/com/guanghe/api/web/controller/manage/HomeController.java
+++ src/main/java/com/guanghe/api/web/controller/manage/HomeController.java
@@ -1,4 +1,4 @@
-package com.guanghe.api.web.controller;
+package com.guanghe.api.web.controller.manage;
 
 import com.guanghe.api.entity.bo.*;
 import com.guanghe.api.entity.dto.ResultDTOBuilder;
@@ -30,17 +30,49 @@ public class HomeController extends BaseCotroller {
     @RequestMapping("/info")
     public void info(HttpServletResponse response){
         List<BannerBo> bannerBo= bannerService.queryBannerInfo();
+        if (bannerBo==null){
+            String json = JsonUtils.getJsonString4JavaPOJO(ResultDTOBuilder.failure("0000001"));
+            safeTextPrint(response, json);
+        }
         List<CompanyIntroductionBo> companyIntroductionBo =infoService.queryCompanyIntroductionInfo();
+        if (companyIntroductionBo==null){
+            String json = JsonUtils.getJsonString4JavaPOJO(ResultDTOBuilder.failure("0000001"));
+            safeTextPrint(response, json);
+        }
         List<NewsInformationBO> newsInformationBO = infoService.queryNewsInformationInfo();
+        if (newsInformationBO==null){
+            String json = JsonUtils.getJsonString4JavaPOJO(ResultDTOBuilder.failure("0000001"));
+            safeTextPrint(response, json);
+        }
         List<ImageBo> imageBo =infoService.queryImageInfo();
+        if (imageBo==null){
+            String json = JsonUtils.getJsonString4JavaPOJO(ResultDTOBuilder.failure("0000001"));
+            safeTextPrint(response, json);
+        }
         PrivateConsultantBo privateConsultantBo=infoService.queryPrivateConsultantInfo();
+        if (privateConsultantBo==null){
+            String json = JsonUtils.getJsonString4JavaPOJO(ResultDTOBuilder.failure("0000001"));
+            safeTextPrint(response, json);
+        }
         WealthManagementBo wealthManagementBo =infoService.queryWealthManagementInfo();
+        if (wealthManagementBo==null){
+            String json = JsonUtils.getJsonString4JavaPOJO(ResultDTOBuilder.failure("0000001"));
+            safeTextPrint(response, json);
+        }
         BusinessSchoolBo businessSchoolBo=infoService.queryBusinessSchoolInfo();
+        if(businessSchoolBo==null){
+            String json = JsonUtils.getJsonString4JavaPOJO(ResultDTOBuilder.failure("0000001"));
+            safeTextPrint(response, json);
+        }
          List<Object> list =new ArrayList<Object>();
         list.add(privateConsultantBo);
         list.add(wealthManagementBo);
         list.add(businessSchoolBo);
         List<PrivateClubBo> privateClubBo =infoService.queryPrivateClub();
+        if (privateClubBo==null){
+            String json = JsonUtils.getJsonString4JavaPOJO(ResultDTOBuilder.failure("0000001"));
+            safeTextPrint(response, json);
+        }
         // simple code
         JSONObject result = new JSONObject();
         result.put("banner", bannerBo);   //image：src/image  url//http：//baidu.com
